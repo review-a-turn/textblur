c662422b49bdda2fe550c4a0d09242b71372f2af
review-a-turn <leviachan@protonmail.com>
2026-03-01 21:59:12 -0500

    Support different result folder

--- file_utils.py
+++ file_utils.py
@@ -47,8 +47,8 @@ def saveResult(img_file, img, boxes, dirname='./result/', verticals=None, texts=
         filename, file_ext = os.path.splitext(os.path.basename(img_file))
 
         # result directory
-        res_file = dirname + "res_" + filename + '.txt'
-        res_img_file = dirname + "res_" + filename + '.jpg'
+        res_file = os.path.join(dirname, "res_" + filename + '.txt')
+        res_img_file = os.path.join(dirname, "res_" + filename + '.jpg')
 
         if not os.path.isdir(dirname):
             os.mkdir(dirname)
